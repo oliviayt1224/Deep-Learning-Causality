929cd36015c3123e18138372b259f49e3d8e32d5
Olivia <oliviaytli@163.com>
2022-08-11 00:03:59 +0100

    Convert into functions.

--- MLP.py
+++ MLP.py
@@ -1,6 +1,6 @@
 from tensorflow import keras
 from tensorflow.keras import layers
-from data import *
+from data_generation import *
 
 
 class LossHistory(keras.callbacks.Callback):
@@ -10,29 +10,45 @@ class LossHistory(keras.callbacks.Callback):
     def on_batch_end(self, batch, logs={}):
         self.losses.append(logs.get('loss'))
 
-
-def data_for_MLP(dataset, lag):
-
-    dataset["Y_lagged"] = dataset["Y"].shift(periods=lag)
-    dataset = dataset.dropna(axis=0, how='any')
-    inseqX = np.array(dataset["X_lagged"])
+def data_for_MLP_Y(dataset):
     seqY = np.array(dataset["Y"])
+    inseqY = np.array(dataset["Y_lagged"])
+    X = np.hstack((inseqY))
+    Y = seqY.reshape((len(seqY), 1))
+
+    return X,Y
+
+def data_for_MLP_XY(dataset):
+    inseqX = np.array(dataset["X_lagged"])
     inseqY = np.array(dataset["Y_lagged"])
 
     inseqX = inseqX.reshape((len(inseqX), 1))
     inseqY = inseqY.reshape((len(inseqY), 1))
 
-    if (dataset.shape[1] == 4):
-        inseqZ = np.array(dataset["Z_lagged"])
-        inseqZ = inseqZ.reshape((len(inseqZ), 1))
-        X = np.hstack((inseqX, inseqY, inseqZ))
+    seqY = np.array(dataset["Y"])
 
-    if (dataset.shape[1] == 3):
-        X = np.hstack((inseqX, inseqY))
-
+    X = np.hstack((inseqX, inseqY))
     Y = seqY.reshape((len(seqY), 1))
 
-    return X, Y
+    return X,Y
+
+
+def data_for_MLP_XYZ(dataset):
+
+    inseqX = np.array(dataset["X_lagged"])
+    inseqY = np.array(dataset["Y_lagged"])
+    inseqZ = np.array(dataset["Z_lagged"])
+
+    inseqX = inseqX.reshape((len(inseqX), 1))
+    inseqY = inseqY.reshape((len(inseqY), 1))
+    inseqZ = inseqZ.reshape((len(inseqZ), 1))
+
+    seqY = np.array(dataset["Y"])
+
+    X = np.hstack((inseqX, inseqY, inseqZ))
+    Y = seqY.reshape((len(seqY), 1))
+
+    return X,Y
 
 
 def MLP(X,Y):
@@ -45,10 +61,14 @@ def MLP(X,Y):
     history = LossHistory()
     model.fit(X, Y, validation_split=0.33, epochs=150, batch_size=10,callbacks=[history])
 
+
     return history.losses[-1]
 
 dataset = coupled_wiener_process(T=1, N=1000, alpha=0.5, lag=5, seed1=None, seed2=None)
 print(dataset.shape[1])
-X, Y = data_for_MLP(dataset,5)
+X, Y = data_for_MLP_XY(dataset)
 loss = MLP(X,Y)
 print(loss)
+
+# predict
+# split out for both linear and non-linear, fix dataset
