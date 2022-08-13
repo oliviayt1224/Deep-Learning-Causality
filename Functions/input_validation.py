7fc8a09dd8a203ae9582ed15c98ffa7776326a7a
Olivia <oliviaytli@163.com>
2022-08-13 02:32:05 +0100

    Create file for input validations.

---
+++ Functions/input_validation.py
@@ -0,0 +1,46 @@
+def validation_coeff(coeff):
+    try:
+        float(coeff)
+
+    except Exception:
+        raise ValueError("The value of the coefficient should be a float. Please try again.")
+
+    if float(coeff) <= 0 or float(coeff) >= 1:
+        raise ValueError("The value of the coefficient should be in range of (0,1). Please try again.")
+
+    return float(coeff)
+
+def validation_T(T):
+    try:
+        float(T)
+
+    except Exception:
+        raise ValueError("The value of T should be a float. Please try again.")
+
+    if float(T) <= 0:
+        raise ValueError("The value of T should be larger than zero. Please try again.")
+
+    return float(T)
+
+
+def validation_N(N):
+    if N <= 0:
+        raise ValueError("The value of N should be larger than zero. Please try again.")
+
+
+def validation_lag(lag):
+    if lag <= 0:
+        raise ValueError("The value of lag should be larger than zero. Please try again.")
+
+
+def validation_N_lag(N, lag):
+    if N <= lag:
+        raise ValueError("The value of lag should be smaller than N. Please try again.")
+
+
+def validation_num_exp(num_exp):
+    if num_exp <= 0:
+        raise ValueError("The number of experiments should be larger than zero. Please try again.")
+
+
+
